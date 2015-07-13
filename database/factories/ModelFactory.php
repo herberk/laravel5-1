eb54d1e9c5d60224e32c0df7a31ba4c06266d509
Hermann Berkhoff <berkhoff@asatemuco.cl>
2015-07-13 11:51:22 -0400

    Este es el prmer commot del cursp laravel 5.1

---
+++ database/factories/ModelFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+$factory->define(App\User::class, function ($faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->email,
+        'password' => str_random(10),
+        'remember_token' => str_random(10),
+        'role' => $faker->randomElement(['user','editor'])
+    ];
+});
---
+++ resources/lang/es/auth.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+  'errors_title'    => 'Por favor corrige los siguientes errores',
+  'login_title'     => 'Inicio de sesión',
+  'login_button'    => 'Iniciar sesión',
+  'register_title'  => 'Registro',
+  'register_button' => 'Regístrate',
+  'remember'        => 'Recuerdane',
+  'forgot_link'     => 'Olvidaste tu Contraseña ?',
+  'Welcome'         => 'Bienvenido a Inicio'
+);
